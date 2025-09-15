a0e2088a4e35fb8b4e4808e9b695fa33420b2410
Seba-SL <slazo@fi.uba.ar>
2025-09-15 11:35:49 -0300

    Tiempos pedidos V(x) = 0

--- script_tp1.py
+++ script_tp1.py
@@ -9,19 +9,22 @@ h = 6.62607015e-34       # J*s (constante de Planck)
 
 # --- Parámetros espaciales ---
 Lx = 240e-9   # longitud del dominio [m]
-Nx = 1200    # número de puntos
+
 dx = 0.05e-9
+
+Nx =int(Lx/dx ) # número de puntos
 x = np.linspace(0, Lx, Nx)
 x_nm = x*1e9  # en nanómetros
 
-Nt = 180000
 dt_max = 0.15 * 2 * m * dx**2 / hbar
 dt = 0.5*dt_max
 
 
+Nt = int(500e-15/dt)
+
 # --- Potencial: escalón en x = 20 nm ---
 V = np.zeros(Nx)
-V[x_nm >= Lx/2 ] = 0.015*eV   # 0.015 eV
+#V[x_nm >= Lx/2 ] = 0.015*eV   # 0.015 eV
 
 # --- Condición inicial: paquete gaussiano ---
 x0 = 110e-9
@@ -53,40 +56,8 @@ norm = np.sqrt(np.trapz(psi_real**2 + psi_imag**2, x))
 psi_real /= norm
 psi_imag /= norm
 
-# --- Potencial (libre en este caso) ---
-V = np.zeros_like(x)
 
-# --- Snapshots ---
-psi_sqr_snapshots = []
-times = []
-
-# --- Evolución temporal ---
-for n in range(Nt):
-
-    # Paso 1: actualizar parte real
-    lap_imag = (np.roll(psi_imag, -1) - 2*psi_imag + np.roll(psi_imag, 1)) / dx**2
-    psi_real_new = psi_real - (hbar*dt/(2*m)) * lap_imag + (dt/hbar)*V*psi_imag
-
-    # Paso 2: actualizar parte imaginaria
-    lap_real = (np.roll(psi_real_new, -1) - 2*psi_real_new + np.roll(psi_real_new, 1)) / dx**2
-    psi_imag_new = psi_imag + (hbar*dt/(2*m)) * lap_real - (dt/hbar)*V*psi_real_new
-
-    # Actualizar variables
-    psi_real, psi_imag = psi_real_new, psi_imag_new
-
-    # Calcular densidad de probabilidad
-    prob_density = psi_real**2 + psi_imag**2
-
-    # Guardar snapshots cada cierto tiempo
-    if n % 500 == 0:
-        psi_sqr_snapshots.append(prob_density.copy())
-        times.append(n*dt*1e15)  # en femtosegundos
-        P_total = np.trapz(prob_density, x)
-        print(f"Paso {n}, Probabilidad total: {P_total:.6f}")
-
-
-
-# --- Cálculo de valores esperados (en el último estado) ---
+# --- Cálculo de valores esperados (Estado inicial) ---
 
 # función de onda compleja Ψ = Re + i Im
 psi = psi_real + 1j * psi_imag
@@ -112,6 +83,43 @@ Ep_mean = np.real(Ep_mean)
 # 5) Energía total <E>
 E_total = Ek_mean + Ep_mean
 
+
+# --- Snapshots ---
+psi_sqr_snapshots = []
+times = []
+
+
+# --- Evolución temporal ---
+for n in range(Nt):
+
+    # Paso 1: actualizar parte real
+    lap_imag = (np.roll(psi_imag, -1) - 2*psi_imag + np.roll(psi_imag, 1)) / dx**2
+    psi_real_new = psi_real - (hbar*dt/(2*m)) * lap_imag + (dt/hbar)*V*psi_imag
+
+    # Paso 2: actualizar parte imaginaria
+    lap_real = (np.roll(psi_real_new, -1) - 2*psi_real_new + np.roll(psi_real_new, 1)) / dx**2
+    psi_imag_new = psi_imag + (hbar*dt/(2*m)) * lap_real - (dt/hbar)*V*psi_real_new
+
+    # Actualizar variables
+    psi_real, psi_imag = psi_real_new, psi_imag_new
+    # # Aplicar máscara absorbente en cada paso
+    # if(n > Nt -1 ): 
+    #     psi_real *= mask
+    #     psi_imag *= mask
+
+    # Calcular densidad de probabilidad
+    prob_density = psi_real**2 + psi_imag**2
+
+    # Guardar snapshots cada cierto tiempo
+    if n % 400 == 0:
+        psi_sqr_snapshots.append(prob_density.copy())
+        times.append(n*dt*1e15)  # en femtosegundos
+        P_total = np.trapz(prob_density, x)
+        print(f"Paso {n}, Probabilidad total: {P_total:.6f}")
+
+
+
+
 print("\n--- Valores esperados (último estado) ---")
 print(f"<x>   = {x_mean:.3e} m")
 print(f"<p>   = {p_mean:.3e} kg·m/s")
@@ -120,22 +128,30 @@ print(f"<Ep>  = {Ep_mean/eV:.3f} eV")
 print(f"<E>   = {E_total/eV:.3f} eV")
 
 
+# --- Graficar snapshots en tiempos específicos ---
+target_times = [0, 30, 80, 450]  # en femtosegundos
+tolerance = 0.5  # tolerancia en fs para encontrar el snapshot más cercano
 
-# --- Graficar snapshots ---
+plt.figure(figsize=(8,5))
+
 for psi_sqr, t in zip(psi_sqr_snapshots, times):
-    plt.plot(x*1e9, psi_sqr, label=f"t = {t:.1f} fs",alpha = 0.9, linewidth = 3)
+    # revisar si t está cerca de alguno de los tiempos objetivo
+    if any(abs(t - T) < tolerance for T in target_times):
+        plt.plot(x*1e9, psi_sqr, label=fr"$|\Psi|^2, \; t = {t:.0f}\ \mathrm{{fs}}$", alpha=0.9, linewidth=3)
+
+plt.plot(x*1e9,V,label = f"V(x) = {V[0]} eV", linestyle = "--", color ="gray", linewidth = 2.5)
 
 textstr = (
     f'P_total = {P_total:.4f}\n'
     f'ra = {ra:.4f}\n'
     f'Δt = {dt:.2e} s\n'
     f'Δx = {dx:.2e} m\n'
-    f'\n--- Valores esperados (último estado) ---\n'
-    f'<x>   = {x_mean:.3e} m\n'
-    f'<p>   = {p_mean:.3e} kg·m/s\n'
-    f'<Ek>  = {Ek_mean/eV:.3f} eV\n'
-    f'<Ep>  = {Ep_mean/eV:.3f} eV\n'
-    f'<E>   = {E_total/eV:.3f} eV\n'
+    # f'\n--- Valores esperados (t = 0) ---\n'
+    # f'<x>   = {x_mean:.3e} m\n'
+    # f'<p>   = {p_mean:.3e} kg·m/s\n'
+    # f'<Ek>  = {Ek_mean/eV:.3f} eV\n'
+    # f'<Ep>  = {Ep_mean/eV:.3f} eV\n'
+    # f'<E>   = {E_total/eV:.3f} eV\n'
 
 )
 
