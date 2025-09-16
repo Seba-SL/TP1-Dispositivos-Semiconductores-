b0ee02e363cbd6da63341174e9a03e5040ad735c
Seba-SL <slazo@fi.uba.ar>
2025-09-16 10:53:47 -0300

    Se agrego el producto de incertidumbres en valores esperados

--- script_tp1.py
+++ script_tp1.py
@@ -59,12 +59,12 @@ V = np.zeros(Nx)
 # Escalón de potencial
 
 # 0.025 eV
-#V[x < Lx/2] = 0*eV
-#V[x >= Lx/2] =0.025 * eV  # 0.025 eV a partir de x = Lx/2
+V[x < Lx/2] = 0*eV
+V[x >= Lx/2] =0.025 * eV  # 0.025 eV a partir de x = Lx/2
 
 # 0.25 eV
-V[x < Lx/2] = 0*eV
-V[x >= Lx/2] =0.25 * eV  # 0.25 eV a partir de x = Lx/2
+#V[x < Lx/2] = 0*eV
+#V[x >= Lx/2] =0.25 * eV  # 0.25 eV a partir de x = Lx/2
 
 
 # # Potencial barrera
@@ -100,6 +100,7 @@ Ep_means = []
 E_totals = []
 deltas_x = []
 deltas_p = []
+productos_deltas_normalizados = []
 
 # --- Snapshots ---
 psi_sqr_snapshots = []
@@ -159,12 +160,18 @@ for n in range(Nt):
             p_mean_2 = 2 * m * Ek_mean
             E_total = Ek_mean + Ep_mean
 
+
             # Incertidumbre de x: delta x = 2 * sqrt(<x^2> - <x>^2)
             delta_x = 2 * np.sqrt(x_mean_2 - x_mean**2)
 
+            
 # Incertidumbre de p: delta p = 2 * sqrt(<p^2> - <p>^2)
 # We use np.real() to handle the complex numbers properly, as p_mean is complex.
             delta_p = 2 * np.sqrt(np.real(p_mean_2) - np.real(p_mean)**2)
+
+            
+            producto_deltas_normalizado = (delta_x*delta_p)/(hbar/2)
+            
 
 
             # Guardar en las listas
@@ -175,6 +182,7 @@ for n in range(Nt):
             E_totals.append(E_total)
             deltas_x.append(delta_x)
             deltas_p.append(delta_p)
+            productos_deltas_normalizados.append(producto_deltas_normalizado)
 
     # Guardar snapshots cada cierto tiempo
     if n % 400 == 0:
@@ -193,7 +201,7 @@ for psi_sqr, t in zip(psi_sqr_snapshots, times):
         plt.plot(x*1e9, psi_sqr, label=fr"$|\Psi|^2, \; t = {t:.0f}\ \mathrm{{fs}}$", alpha=0.9, linewidth=3)
 
 
-
+#Casos Escalones y Particula libre 
 plt.plot(x*1e9,(V/eV)*1e9,linestyle = "--", label=f"V(x = Lx/2) = {V[idx]/eV:.3f} eV " , color ="gray", linewidth = 3 )
   
 #caso barreras   
@@ -201,8 +209,8 @@ plt.plot(x*1e9,(V/eV)*1e9,linestyle = "--", label=f"V(x = Lx/2) = {V[idx]/eV:.3f
   
     
     
-for T, x_val, p_val, Ek_val, Ep_val, E_val, delta_x, delta_p in zip(target_times, x_means, p_means, Ek_means, Ep_means, E_totals, deltas_x,deltas_p):
-    print(f"t = {T} fs: <x>={x_val:.3e} m, <p>={p_val:.3e} kg·m/s, <Ek>={Ek_val/eV:.3f} eV, <Ep>={Ep_val/eV:.3f} eV, <E>={E_val/eV:.3f} eV ", fr"$\delta x = {delta_x}$", fr"$\delta p = {delta_p}$")
+for T, x_val, p_val, Ek_val, Ep_val, E_val, delta_x, delta_p in zip(target_times, x_means, p_means, Ek_means, Ep_means, E_totals, deltas_x,deltas_p,productos_deltas_normalizados):
+    print(f"t = {T} fs: <x>={x_val:.3e} m, <p>={p_val:.3e} kg·m/s, <Ek>={Ek_val/eV:.3f} eV, <Ep>={Ep_val/eV:.3f} eV, <E>={E_val/eV:.3f} eV ", fr"$\delta x = {delta_x}$", fr"$\delta p = {delta_p}$",fr"$deltas/(hbar/2) = {producto_deltas_normalizado}$" )
 
 
 #plt.plot(x*1e9, V/eV, linestyle='--', color='gray', label="V(x) [eV]")
